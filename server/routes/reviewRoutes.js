4c77c9c4c3882b9249f42b71b89fd20908acdb05
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2023-03-31 03:07:52 +0530

    Updated Meal Review Route

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAReview, getAllReviews, getReviewsOfAMeal, createReview, updateAReview, deleteReview} = require("./../controllers/reviewController");
+const {
+  getAReview,
+  getAllReviews,
+  getReviewsOfAMeal,
+  createReview,
+  updateAReview,
+  deleteReview,
+} = require("./../controllers/reviewController");
 
-// router.get("/", (req,res,next) => {
-//     res.status(200)
-// });
 router.get("/", getAllReviews);
-router.get("/meal/:mealId", getReviewsOfAMeal);
-router.get("/:reviewId", getAReview);
+router.get("/meal", getReviewsOfAMeal);
 
+router.get("/:reviewId", getAReview);
 router.post("/", createReview);
 router.patch("/:reviewId", updateAReview);
 router.delete("/:reviewId", deleteReview);
-
 
 module.exports = router;
