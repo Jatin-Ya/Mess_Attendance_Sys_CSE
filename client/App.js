dd6d28a661b286b2b47e5e54d42a9d523df27e60
Jatin Yadav <jatinyadav20032003@gmail.com>
2023-03-31 03:07:24 +0530

    Added AuthContext

--- client/App.js
+++ client/App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AuthButton from './components/AuthButton';
+import DisplayToken from './components/DisplayToken';
+import LogutButton from './components/LogoutButton';
+import { AuthContextProvider } from './contexts/AuthContext';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to the app.</Text>
-      <AuthButton></AuthButton>
-      <StatusBar style="auto" />
-    </View>
+    <AuthContextProvider>
+      <View style={styles.container}>
+        <Text>Welcome to the app.</Text>
+        <AuthButton></AuthButton>
+        <DisplayToken></DisplayToken>
+        <LogutButton></LogutButton>
+        <StatusBar style='auto' />
+      </View>
+    </AuthContextProvider>
   );
 }
 
