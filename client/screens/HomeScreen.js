1e091c2bbbfe8fd0379323f425239927c5287542
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2023-03-31 23:28:52 +0530

    Added Details to HomeScreen

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,13 +1,28 @@
-import { View,Text } from "react-native"
-import DisplayToken from "../components/DisplayToken";
+import { View, Text, Image } from "react-native";
+import useAuthContext from "../hooks/useAuthContext";
 
 const HomeScreen = () => {
+  const { user } = useAuthContext();
+
   return (
     <View>
       <Text>Home Screen</Text>
-      <DisplayToken></DisplayToken>
+      <Text>Name: {user.name}</Text>
+      <Text>Roll Number: {user.rollNumber}</Text>
+      <Text>Email: {user.email}</Text>
+      <Text>
+        Residence: {user.roomNumber}, {user.hostel}
+      </Text>
+      <Text>Picture: {user.picture}</Text>
+      <Image
+        source={{
+          uri: user.picture,
+        }}
+      />
+
+      {/* <DisplayToken></DisplayToken> */}
     </View>
-  )
-}
+  );
+};
 
 export default HomeScreen;
