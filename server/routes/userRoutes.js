9f037311343909a527a76107f3999cd81ea46397
navaneeth-bysani <bysaninavaneeth@gmail.com>
2023-03-31 12:46:11 +0530

    added quantity field in meal model and get attendance function

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,7 @@ const userController = require("../controllers/userController");
 const Router = express.Router();
 
 Router.post("/addMessBalance", userController.addMessBalance);
+Router.get("/attendance-self", userController.getAttendanceSelf);
 Router.get("/getAttendance", userController.generateMessAttendanceExcel);
 Router.post("/demo", userController.addMealToUser);
 
