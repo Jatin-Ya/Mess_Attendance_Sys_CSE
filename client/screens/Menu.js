77caf6b7afdd5ccd0a9099c529d47b7c311b5e1b
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2023-04-01 12:33:57 +0530

    Menu Part 1 Done

--- client/screens/Menu.js
+++ client/screens/Menu.js
@@ -1,5 +1,12 @@
 import React, { Component, useEffect, useState } from "react";
-import { StyleSheet, View, Text, Button, Image } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 import axios from "../utils/axios";
 import useAuthContext from "../hooks/useAuthContext";
@@ -18,19 +25,49 @@ function Menu() {
       .catch((err) => console.log(err));
   }, []);
 
+  const getName = () => {
+    let ans = user.name;
+
+    ans =
+      user.name.charAt(0).toUpperCase() +
+      user.name.split(" ")[0].slice(1).toLowerCase();
+
+    return ans;
+  };
+
   return (
     <View
       style={{
         background: "#E5E5E5",
       }}
     >
-      <Text style={styles.welcome}> Welcome {user.name}</Text>
+      <Text style={styles.welcome}>Welcome {getName()} !</Text>
       <View style={styles.rect}>
         <Text style={styles.upcoming}> Upcoming Meal </Text>
         <Image source={require("../assets/meal.png")} style={styles.vec} />
-        <Text>Chicken Biryani, Raita, Veg Pulav, Papad, Curry</Text>
-        <Image source={require("../assets/Khana.png")} style={styles.khana} />
-        <Text style={styles.fullMenu}>View Full Menu</Text>
+        <Text style={styles.items}>
+          Chicken Biryani, Raita, Veg Pulav, Papad, Curry
+        </Text>
+        <Image
+          source={require("../assets/Khana.png")}
+          style={[styles.khana, { resizeMode: "contain" }]}
+        />
+
+        <Text
+          onPress={() => {
+            console.log("Display full menu....");
+          }}
+          style={styles.fullMenu}
+        >
+          View Full Menu
+        </Text>
+        <Image
+          onPress={() => {
+            console.log("Display full menu icons....");
+          }}
+          source={require("../assets/arrow.png")}
+          style={styles.arrow}
+        />
       </View>
 
       <View>
